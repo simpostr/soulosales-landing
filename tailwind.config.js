b44e435fae6493d11aecf877bab14700f7cedc5a
simpostr <jack.anthonydamian@gmail.com>
2025-05-30 15:51:29 -0400

    not much

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: ["class"],
-    content: [],
+    content: [
+'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}', 
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+],
   theme: {
   	extend: {
   		borderRadius: {
